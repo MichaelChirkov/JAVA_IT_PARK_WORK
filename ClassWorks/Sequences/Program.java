06767157db0ea0ad5a0069bc87eeee89c9c887ee
MichaelChirkov <chirkovmtest@yandex.ru>
2017-03-11 16:35:36 +0300

    add classworks

---
+++ ClassWorks/Sequences/Program.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Program {
+public static void main(String[] args) {
+	Scanner scanner = new Scanner (System.in);
+	int a;
+	int s=0;
+	a = scanner.nextInt();
+	while (a != -1) {
+			s = s + a;
+			a = scanner.nextInt();
+		}
+		System.out.println(s);
+	}
+}
