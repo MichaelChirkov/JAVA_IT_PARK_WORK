9cf4e8dc9763e8ee807195bb761357186a2760f5
MichaelChirkov <chirkovmtest@yandex.ru>
2017-03-13 19:00:26 +0300

    add homework

--- HomeWorks/Task14/Program.java
+++ HomeWorks/Task14/Program.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Program{
 
-	public static String fun(int b){
+	public static boolean fun(int b){
 		String c;
 		int k=0;
 		for (int i = 2; i < b ; i++){				
@@ -10,22 +10,21 @@ class Program{
 				k++;
 			}	
 			System.out.println();		
-			if(k==0) 				
-			//System.out.println("Chislo " + b + " prostoe\n");
-			return	c="Chislo yavlyaetsya prostbim";
-			else
-			//System.out.println("Chislo " + b + "  ne yavlyaetsya prostbim \n");
-			return	c="Chislo ne yavlyaetsya prostbim";
-
-	}
+			if(k==0) 			
+			return	true;
+			else			
+			return	false;
+		}
 
 	public static void main(String[] args) {
-
 		Scanner scanner = new Scanner(System.in);
 		int a;
 		System.out.println();
 		a = scanner.nextInt();
-		System.out.println(fun(a));	
+		if(fun(a) == true)
+			System.out.println("Chislo " + a +" yavlyaetsya prostbim");
+		else			
+			System.out.println("Chislo " + a +" ne yavlyaetsya prostbim");
 		System.out.println();	
 	}
 }
