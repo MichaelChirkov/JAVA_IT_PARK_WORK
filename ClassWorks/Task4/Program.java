588be0de25e51f22b861f4016eed0ce6050ad441
MichaelChirkov <chirkovmtest@yandex.ru>
2017-03-16 21:02:10 +0300

    add class takk4

--- ClassWorks/Task4/Program.java
+++ ClassWorks/Task4/Program.java
@@ -2,40 +2,29 @@ import java.util.Random;
 class Program {
 	public static void main(String[] args) {
 		int ages[] = new int[1000];
-		int num=0;
 		Random random = new Random();
+		int kol[] = new int[121];
 
 		for(int i = 0; i < ages.length; i++) {
 			ages[i] = random.nextInt(120);
 		}
 
-		for(int i = 0; i < 100; i++) {
+		for(int i = 0; i < 100; i++) { // 100
 			System.out.println(ages[i]);
 		}
 
-		// вывести на экран количество людей, с возрастами от 0 до 120
-		int b[][]=new int[2][1000];
-		for (int i=0;i<b.length;i++){ //
-			b[0][i]=ages[i];
-			b[1][i]=0;
-		}
-
-		
-			for (int i=0;i<b.length;i++){
-			if (i==b[0][i]){
-				b[1][i]+=1;
+		for (int j = 0; j<121; j++){ // возраста от 0 до 120
+				for(int i = 0; i < ages.length; i++){ // подсчет по всему массиву ages[]
+					if (j == ages[i]){
+					kol[j] += 1;
+					}
 			}
+		}
+		for (int i=0; i<121; i++)
+			System.out.println(i + " - " +kol[i] );
 		
-
-		}
-
-
-		for (int i=0;i<2;i++){
-			for (int j=0;j<1000;j++){
-				System.out.println(b[i][j]);
-			}
-			System.out.println();
-		}
-
 	}
+		// вывести на экран количество людей, с возрастами от 0 до 120
+		
+
 }
