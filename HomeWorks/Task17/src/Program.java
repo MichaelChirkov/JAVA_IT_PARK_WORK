2fd3b5bc0789d2a9c42356a0145002e80e8cdeb8
MichaelChirkov <chirkovmtest@yandex.ru>
2017-03-26 01:11:56 +0300

    Add homework

--- HomeWorks/Task17/src/Program.java
+++ HomeWorks/Task17/src/Program.java
@@ -5,14 +5,20 @@ public class Program {
     public static void main(String[] args) {
 
         Human humans[] = new Human[] {
-                new Human("Misha",20),
-                new Human("Oleg",25),
-                new Human("Petr",15),
+                new Human("Миша",20),
+                new Human("Олег",25),
+                new Human("Петр",15),
+                new Human("Иван",23),
+                new Human("Костя",25),
+                new Human("Игорь",17),
+                new Human("Анастасия",30),
+                new Human("Ольга",36),
+                new Human("Настя",20),
                 new Human(),
         };
 
-        humans[3].setName("Masha");
-        humans[3].setAge(19);
+        humans[9].setName("Маша"); // использовал другой способ инициализации для тренировки
+        humans[9].setAge(19);
 
         for (int i = 0; i < humans.length; i++){
             System.out.println("Имя = " + humans[i].getName() + " Возраст = " + humans[i].getAge());
