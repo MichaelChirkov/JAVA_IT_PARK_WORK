96465fcaec99077de369806588e5a918d79cae72
Michael <Chirkovmtest@yandex.ru>
2017-04-01 18:11:59 +0300

    add classWork

---
+++ ClassWorks/Task7/src/Humans.java
@@ -0,0 +1,8 @@
+/**
+ * Created by vclass16 on 01.04.2017.
+ */
+public class Humans extends SuperHero {
+    public Humans( String name, int enemyCount){
+        super(name, enemyCount);
+    }
+}
