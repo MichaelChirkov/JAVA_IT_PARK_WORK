89ecd5879cd3919bc72c6324a2661f1bb2ef2b9a
MichaelChirkov <chirkovmtest@yandex.ru>
2017-04-15 17:48:19 +0300

    add task10

--- ClassWorks/Task10/src/Main.java
+++ ClassWorks/Task10/src/Main.java
@@ -4,20 +4,31 @@
 import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
-        Square square = new Square("Квадратик");
-        Triangle triangle = new Triangle("Эчпочмак");
-        Rectangle rectangle = new Rectangle("Прямоуголничек");
+        Square square = new Square("Square");
+        Triangle triangle = new Triangle("Triangle");
+        Rectangle rectangle = new Rectangle("Rectangle");
         Figures[] figures = { square, triangle, rectangle};
 
         OutputStream outputStream =
                 new FileOutputStream("Z:\\Students\\JAVA_IT_PARK_1\\CHIRKOV\\JAVA_IT_PARK_WORK\\ClassWorks\\Task10\\output.txt");
-
-
+        InputStream input =
+                new FileInputStream("output.txt");
+//запис в файл
             for (int i = 0; i < figures.length; i++) {
                 byte bytesArray[] = figures[i].getName().getBytes();
                 outputStream.write(bytesArray);
                 outputStream.write(10);
 
             }
+
+
+
+// чтение из файла
+        byte bytesFromFile[] = new byte[100];
+        input.read(bytesFromFile);
+
+        for (int i = 0; i < bytesFromFile.length; i++) {
+            System.out.print((char)bytesFromFile[i]);
+        }
     }
 }
