b23491e3c8da512a32ffa4f2a9437c5ce85baf02
MichaelChirkov <chirkovmtest@yandex.ru>
2017-04-17 23:11:02 +0400

    upd

--- HomeWorks/Task21/src/Program.java
+++ HomeWorks/Task21/src/Program.java
@@ -3,20 +3,17 @@ import java.io.IOException;
 /**
  * Created by Michael on 14.04.2017.
  */
+
+
 public class Program {
+    public static int charsArrayToInteger(char chars[]){
+        String str = new String(chars);
+        int result = Integer.valueOf(str);
+        return result;
+    }
 
-    public static void main(String[] args) throws IOException {
-        char[] charsArray = new char[50];
-        int[] array = new int[50];
-        int i = 0;
-        char character;
-        do{
-            array[i] = System.in.read();
-            charsArray[i] = (char)array[i];
-            i++;
-        } while (charsArray[i-1] != '.'); //пока не 1
-
-        String str = new String(charsArray);
-        System.out.println(str);
+    public static void main(String[] args){
+        char[] charsArray = { '3', '2', '1'};
+        System.out.println(charsArrayToInteger(charsArray));
     }
 }
